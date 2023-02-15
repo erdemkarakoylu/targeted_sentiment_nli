d5815466cfc0ddca543f22b4d147d4b675cd4b7f
Erdem Karakoylu <ekarakoylu@researchinnovations.com>
2023-02-15 11:37:30 -0500

    data directory and dataframe fixtures

--- tests/test_nli_datamodule.py
+++ tests/test_nli_datamodule.py
@@ -1,16 +1,32 @@
+from pathlib import Path
 import sys
+
+import numpy as np
 #sys.path.append('../')
 import pandas as pd
-import numpy as np
-from sklearn.datasets import make_classification
 import pytest
+from sklearn.datasets import make_classification
 
 from src.nli_datamodule import split_data
 
+
+@pytest.fixture
+def dataframe_dir():
+    return Path('../data/sentihood')
+
 @pytest.fixture
 def dataframe()->pd.DataFrame:
+    return pd.read_json(dataframe_dir/'sentihood_dev.json')
+
+def test_data_exists(dataframe_dir):
+    assert dataframe_dir.exists()
+    assert (dataframe_dir/'sentihood_dev.json').exists()
+    assert (dataframe_dir/'sentihood_test.json').exists()
+    assert (dataframe_dir/'sentihood_train.json').exists()
+
+def test_dataframe_columns(dataframe):
     pass
-    
+
 def test_split_data():
     pass
 
