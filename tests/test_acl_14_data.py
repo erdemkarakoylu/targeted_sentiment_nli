959f7bb8ed9cc1f1a5279d87c9b9de590d3ba1e3
Erdem Karakoylu <ekarakoylu@researchinnovations.com>
2023-02-17 11:15:13 -0500

    set data testing apart

---
+++ tests/test_acl_14_data.py
@@ -0,0 +1,10 @@
+from src.constants import DATA_COLUMNS
+
+def test_duplicate_rows(acl_14_dataframe):
+    pass
+
+def test_dataframe_column_dtypes(acl_14_dataframe):
+    pass
+
+def test_dataframe_columns(acl_14_dataframe):
+    pass
---
+++ tests/test_sentfin_data.py
@@ -0,0 +1,10 @@
+from src.constants import DATA_COLUMNS
+
+def test_duplicate_rows(sentfin_dataframe):
+    pass
+
+def test_dataframe_column_dtypes(sentfin_dataframe):
+    pass
+
+def test_dataframe_columns(sentfin_dataframe):
+    pass
