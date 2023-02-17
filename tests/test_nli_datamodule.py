99d0389e3deb4dadfd3676448c3358e18747543d
Erdem Karakoylu <ekarakoylu@researchinnovations.com>
2023-02-17 11:15:38 -0500

    datamodule testing on its own

--- tests/test_nli_datamodule.py
+++ tests/test_nli_datamodule.py
@@ -1,34 +1,8 @@
-from pathlib import Path
-import sys
-
 import numpy as np
-#sys.path.append('../')
 import pandas as pd
-import pytest
-from sklearn.datasets import make_classification
 
-from src.nli_datamodule import split_data
+from src.nli_datamodule import NLIDataModule
 
-
-@pytest.fixture
-def data_directory():
-    return Path('../data/sentihood')
-
-@pytest.fixture
-def dataframe(data_directory)->pd.DataFrame:
-    return pd.read_json(data_directory/'sentihood_dev.json')
-
-def test_data_exists(data_directory):
-    assert data_directory.exists()
-    assert (data_directory/'sentihood_dev.json').exists()
-    assert (data_directory/'sentihood_test.json').exists()
-    assert (data_directory/'sentihood_train.json').exists()
-
-def test_dataframe_columns(dataframe):
-    pass
-
-def test_split_data():
-    pass
 
 def test_set_tokenizer():
     pass
