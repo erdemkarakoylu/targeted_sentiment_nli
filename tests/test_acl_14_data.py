dd0292f0635ce525b56625bd62184eac034a5e28
Erdem Karakoylu <ekarakoylu@researchinnovations.com>
2023-02-22 10:40:44 -0500

    acl data tests completed

--- tests/test_acl_14_data.py
+++ tests/test_acl_14_data.py
@@ -1,10 +1,13 @@
+"""Tests in this script test the data located in data/sentfin."""
+
 from src.constants import DATA_COLUMNS
 
 def test_duplicate_rows(acl_14_dataframe):
-    pass
+    duplicated_df = acl_14_dataframe[acl_14_dataframe.duplicated()]
+    assert duplicated_df.empty
 
-def test_dataframe_column_dtypes(acl_14_dataframe):
-    pass
+def test_dataframe_column_dtypes(acl_14_dataframe, df_data_types_dict):
+    assert acl_14_dataframe.dtypes.to_dict() == df_data_types_dict
 
 def test_dataframe_columns(acl_14_dataframe):
-    pass
+    assert set(acl_14_dataframe.columns.tolist()) == set(DATA_COLUMNS)
